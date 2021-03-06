a9c151841f77856ff787e67c3876939e6147c362
darren <geng.xuan@belle.com.cn>
2021-03-06 12:21:26 +0800

    init project

---
+++ api-commons/src/main/java/com/darrenkeng/springcloud/entities/CommonResult.java
@@ -0,0 +1,30 @@
+package com.darrenkeng.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @description:
+ * @author: darren
+ * @company: wonhigh
+ * @create: 2021-03-06
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T> implements Serializable {
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public CommonResult(Integer code, String message) {
+        this(code, message, null);
+    }
+
+}
